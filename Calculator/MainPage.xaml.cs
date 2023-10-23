f1f919bc17107d450591def1e39a20b7bcd3204d
DBOYttt <andrzejn041@gmail.com>
2023-10-23 23:57:39 +0200

    first commit

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,169 @@
+﻿namespace Calculator;
+
+public partial class MainPage : ContentPage
+{
+    
+    public MainPage()
+    {
+        InitializeComponent();
+        OnClear(this, null); // dla bezpieczeństwa restartuje wartości początkowe
+
+    }
+
+    string currentEntry = "";
+    int currentState = 1;
+    string mathOperator;
+    double firstNumber, secondNumber;
+    string decimalFormat = "N0";
+
+    private async Task AnimateButton(Button button)
+    {
+        await button.ScaleTo(0.95, 50); // Animacja przycisków await użyam do czekania aż przycisk zostanie zeskalowany(inaczej mówiąc oczekuje na np.button.ScaleTo)
+        await button.FadeTo(0.5, 50);  
+        await button.ScaleTo(1, 50);   
+        await button.FadeTo(1, 50);    
+    }
+
+
+    async void OnSelectNumber(object sender, EventArgs e) // wypisanie klikniętej cyfry na wyświetlaczu 
+    {
+
+        Button button = (Button)sender;
+        await AnimateButton(button); // animacja przycisku 
+
+        string pressed = button.Text;
+
+        currentEntry += pressed; // zliczanie aktualnych danych wejściowych
+
+        if ((this.resultText.Text == "0" && pressed == "0")
+            || (currentEntry.Length <= 1 && pressed != "0") // jeśli na ekranie jest na ekranie wartość dłogości 1 bądź mniej i jest inny od zera to czyścimy wyświetlacz tnz. chodzi o to że jak jest 0 i wpiszemy 1 to jest jeden nie 01
+            || currentState < 0)
+        {
+            this.resultText.Text = "";
+            if (currentState < 0)
+                currentState *= -1;
+        }
+
+        if (pressed == "." && decimalFormat != "N2") // liczby po kropce a dokładniej ich format, nie wiem co znaczy N2 znalazłem to gdzieś na internecie (●'◡'●)
+        {
+            decimalFormat = "N2";
+        }
+
+        this.resultText.Text += pressed;
+    }
+
+    async void OnSelectOperator(object sender, EventArgs e) // używanie operatora matematycznego, ps funkcja asynchroniczna na potrzebe użycia await
+    {
+        LockNumberValue(resultText.Text); // pobieranie aktualnej wartości kalkulatora
+
+        currentState = -2;
+        Button button = (Button)sender;
+        await AnimateButton(button);
+        string pressed = button.Text;
+        mathOperator = pressed;  // odwołanie (albo raczej przekazanie) operatora do pliku Calculator.cs          
+    }
+
+    private void LockNumberValue(string text) // praktycznie to samo co przekształcanie stringa w liczbe 
+    {
+        // TO:DO dodać wyniki po przecinku
+        double number;
+        if (double.TryParse(text, out number))
+        {
+            if (currentState == 1)
+            {
+                firstNumber = number;
+            }
+            else
+            {
+                secondNumber = number;
+            }
+
+            currentEntry = string.Empty;
+        }
+    }
+
+    void OnClear(object sender, EventArgs e) // czyszczenie
+    {
+        firstNumber = 0;
+        secondNumber = 0;
+        currentState = 1;
+        decimalFormat = "N0";
+        this.resultText.Text = "0";
+        currentEntry = string.Empty;
+    }
+
+
+    void OnCalculate(object sender, EventArgs e) //wypisywanie wyniku równania 
+    {
+        if (currentState == 2)
+        {
+            if(secondNumber == 0)
+                LockNumberValue(resultText.Text);
+
+            double result = Calculator.Calculate(firstNumber, secondNumber, mathOperator);
+
+            this.CurrentCalculation.Text = $"{firstNumber} {mathOperator} {secondNumber}";
+
+            this.resultText.Text = result.ToTrimmedString(decimalFormat);
+            firstNumber = result;
+            secondNumber = 0;
+            currentState = -1;
+            currentEntry = string.Empty;
+        }
+    }    
+
+    async void OnNegative(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        await AnimateButton(button);
+        if (currentState == 1)
+        {
+            secondNumber = -1;
+            mathOperator = "×";
+            currentState = 2;
+            OnCalculate(this, null);
+        }
+    }
+
+    async void OnPercentage(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        await AnimateButton(button);
+        if (currentState == 1)
+        {
+            LockNumberValue(resultText.Text);
+            decimalFormat = "N2";
+            secondNumber = 0.01;
+            mathOperator = "×";
+            currentState = 2;
+            OnCalculate(this, null);
+        }
+    }
+
+    async void OnRoot(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        await AnimateButton(button);
+        if (currentState == 1)
+        {
+            LockNumberValue(resultText.Text);
+            mathOperator = "√";
+            currentState = 2;
+            OnCalculate(this, null);
+        }
+    }
+
+    async void OnSquare(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        await AnimateButton(button);
+        if (currentState == 1)
+        {
+            LockNumberValue(resultText.Text);
+            mathOperator = "x²";
+            currentState = 2;
+            OnCalculate(this, null);
+        }
+    }
+
+}
